55e43d7318bb110f15b4b6e4717905da5dfb5a77
saidmoussadek <saiddus24@gmail.com>
2022-07-04 16:27:18 +0200

    pushe

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 const età= parseInt(prompt('Inserire età'));
+const distanza= parseInt(prompt('Inserire distanza'));
+const prezzo= 0.21
+let scontominori= 20%
+let scontoover= 40%
+let prezzobiglietto= (distanza * prezzo)
+
+
+
+
+if( età >= 18 && età <= 65);{
+
+    alert('prezzobiglietto')
+
+}else(età > 18){
+    
+    alert('prezzobiglietto - scontominori')
+}
