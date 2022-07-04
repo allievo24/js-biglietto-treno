d458d86bfd58003d90356dbe3bfb592dea6ff68f
saidmoussadek <saiddus24@gmail.com>
2022-07-04 16:48:27 +0200

    pushe

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 const età= parseInt(prompt('Inserire età'));
 const distanza= parseInt(prompt('Inserire distanza'));
 const prezzo= 0.21
-let scontominori= 20%
-let scontoover= 40%
-let prezzobiglietto= (distanza * prezzo)
+
 
 
 
