8fc3ff85dd9ae60101dbbf48ad7ecb49fde96ecb
saidmoussadek <saiddus24@gmail.com>
2022-07-04 17:21:10 +0200

    push

--- js/main.js
+++ js/main.js
@@ -1,16 +1,38 @@
 const età= parseInt(prompt('Inserire età'));
 const distanza= parseInt(prompt('Inserire distanza'));
-const prezzo= 0.21
+const prezzo= 0.21;
+const scontoMinori=20;
+const scontoOver=40;
+const prezzoBiglietto=(distanza * prezzo); 
 
 
 
+if(eta < 18) {
+    // calcolo 20%
+    let sconto = prezzoBiglietto * 40 / 100; 
+    let prezzoFinale = prezzoBiglietto - sconto;
+    let prezzoFinaleFormattato =prezzoFinale.toFixed(2);
+    alert('prezzoFinaleFormattato');
+} else if (eta > 65) {
+    // calcolo 40%
 
-
-if( età >= 18 && età <= 65);{
-
-    alert('prezzobiglietto')
-
-}else(età > 18){
-    
-    alert('prezzobiglietto - scontominori')
+    let sconto = prezzoBiglietto * 65 / 100; 
+    let prezzoFinale = prezzoBiglietto - sconto;
+    let prezzoFinaleFormattato =prezzoFinale.toFixed(2);
+    alert('prezzoFinaleFormattato');
+} else {
+    // nessuna operazione
+    alert('prezzobiglietto');
 }
+
+
+/* if( età >= 18 && età <= 65);{
+
+    alert('prezzobiglietto');
+
+} else if (età < 18){
+    
+    alert('prezzobiglietto - scontominori');
+} else if (eta > 65) {
+
+} */
